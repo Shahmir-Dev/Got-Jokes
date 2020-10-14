fd4b6b6550d92862b8fc604010bf6ec3de0f45be
Ray Bray <rbray.bray2@gmail.com>
2020-10-14 14:23:31 -0400

    added index.js file again

--- models/joke.js
+++ models/joke.js
@@ -1,8 +1,3 @@
-// Sequelize (capital) references the standard library
-const Sequelize = require("sequelize");
-// sequelize (lowercase) references our connection to the DB.
-const sequelize = require("../config/connection.js");
-
 module.exports = function(sequelize, DataTypes) {
     const Jokes = sequelize.define("Jokes", {
       // Giving the Author model a name of type STRING
