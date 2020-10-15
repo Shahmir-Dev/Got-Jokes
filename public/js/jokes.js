a3378a16c1a646fc5c914f2724a46bc50d9c146a
Ray Bray <rbray.bray2@gmail.com>
2020-10-15 11:12:43 -0400

    added give me a joke package

--- public/js/jokes.js
+++ public/js/jokes.js
@@ -1,25 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  const giveMeAJoke = require("give-me-a-joke");
 
-    var blogContainer = $("#blog-container");
-    var jokeSection = $("#joke-section");
-
-    $('#g-rated').click(function() {
-        console.log('clicked');
-        console.log('clicked');
-        $.get("/g", function(data) {
-            console.log('clicked!!');
-        });
-
+  $("#g-rated").click(function () {
+    // To get a random dad joke
+    giveMeAJoke.getRandomDadJoke(function (joke) {
+      console.log(joke);
     });
+    console.log("clicked");
+    $.get("/g", function (data) {
+      console.log("clicked!!");
+    });
+  });
 
-    $('#pg-rated').click(function() {
-        console.log('clicked!!');
-    });
-    $('#pg13-rated').click(function() {
-        console.log('clicked!!');
-    });
-    $('#r-rated').click(function() {
-        console.log('clicked!!');
-    });
-
+  $("#pg-rated").click(function () {
+    console.log("clicked!!");
+  });
+  $("#pg13-rated").click(function () {
+    console.log("clicked!!");
+  });
+  $("#r-rated").click(function () {
+    console.log("clicked!!");
+  });
 });
