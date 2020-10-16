6a8aaaad9f1b37699774ab4e0823d8738ec84d57
ShahmirFarooqui <shahmirfarooqui91@gmail.com>
2020-10-16 14:46:59 -0400

    cleaned up page styling

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,17 +1,15 @@
 var path = require('path');
 var giveMeAJoke = require('give-me-a-joke');
 var jokes = require("../models/joke");
-var passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
 // // Routes
 
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
 
     // Each of the below routes just handles the HTML page that the user gets sent to.
 
     // index route loads view.html
-    app.get("/", function(req, res) {
+    app.get("/", function (req, res) {
         // db.Jokes.findAll().then((jokes) => {
         res.render("index");
         // });
